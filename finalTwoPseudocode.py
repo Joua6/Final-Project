b1229ebc5be89a7380932b166fba824640cdef1d
Joua6 <61891602+Joua6@users.noreply.github.com>
2020-03-08 22:58:40 -0500

    Add files via upload

--- finalTwoPseudocode.py
+++ finalTwoPseudocode.py
@@ -5,36 +5,23 @@ Created on Mar 7, 2020
 '''
 #PSEUDOCODE:
 #import turtle
-import turtle
 
 #create function 
-def drawing:
-turtle.setup(width=600, height=500)
-turtle.hideturtle()
-turtle.speed(0)
-turtle.bgcolor('black')
+# set width and height
+#set speed
+#Set look
+
 
 # create variable x and c with zero value
-c = 0
-x = 0
+
 
 # write color for color control. 
 #By writing it in a string the colors changes 
 # multiplying it by a number tells how many times the colors changes 
-colors = ["orange","red","yellow","green","blue"]*6
+
 
 #write loop to give directions  
 # use forward, right, and left to give directions 
 #add values to variables 
-while x < 2000:
-    idx = int(c)
-    color = colors[idx]
-    turtle.color(color)
-    turtle.forward(x)
-    turtle.right(98)
-    x = x + 1.5
-    c = c + 0.1
-   
-# create a way to stop drawing
-turtle.exitonclick()
+
 
